1eaaa5edfc4cb903aaad2c09bd6afece0831dc62
EberthCode <U22221452@utp.edu.pe>
2026-03-08 15:48:45 -0500

    Login_front

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login-page.html',
   styles: ``,
 })
 export class LoginPage {
 
+  fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  async onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    console.log('Intento de login presionado');
+    console.info(this.loginForm.value);
+
+    const { email, password } = this.loginForm.value;
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const userFound = users.find((u: any) => u.email === email && u.password === password);
+
+    if (userFound) {
+      alert('¡Bienvenido de nuevo!');
+    } else {
+      alert('Credenciales incorrectas o usuario no registrado.');
+    }
+  }
 }
