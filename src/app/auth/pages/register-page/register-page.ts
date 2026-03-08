8f9810b471032aff95a78d0744f49995a2417082
EberthCode <U22221452@utp.edu.pe>
2026-03-08 14:42:01 -0500

    Register_front

--- src/app/auth/pages/register-page/register-page.ts
+++ src/app/auth/pages/register-page/register-page.ts
@@ -1,13 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register-page.html',
-  styles: ``,
+  styles: `
+    @keyframes slideAnimation {
+      0% { opacity: 0; transform: scale(1); }
+      5% { opacity: 1; }
+      33% { opacity: 1; }
+      38% { opacity: 0; transform: scale(1.05); }
+      100% { opacity: 0; }
+    }
+    .animate-slideshow {
+      animation: slideAnimation 15s infinite;
+    }
+  `,
 })
 export class RegisterPage {
+
+  readonly images: string[] = [
+    'assets/concert_register.webp',
+    'assets/concert_register_2.webp',
+    'assets/concert_register_3.webp',
+  ];
 
   fb = inject(FormBuilder);
 
